18df79fc600df29a1f6f416472fbf2f7ccc17604
kiruha13 <65366618+kiruha13@users.noreply.github.com>
2023-01-17 08:19:47 +0300

    Update swap.php

--- swap.php
+++ swap.php
@@ -1,11 +1,11 @@
 <?php
 include "db.php";
 
-$arr_key_1 = $_POST['arr_key_1'];
-$arr_key_2 = $_POST['arr_key_2'];
+$arr_key_1 = $_POST['arrkey1'];
+$arr_key_2 = $_POST['arrkey2'];
 
-$elem_1 = mysqli_fetch_row(mysqli_query($db, "SELECT * FROM mass WHERE `keyarr` = '$arr_key_1'"));
-$elem_2 = mysqli_fetch_row(mysqli_query($db, "SELECT * FROM mass WHERE `keyarr` = '$arr_key_2'"));
+$elem_1 = mysqli_fetch_row(mysqli_query($db, "SELECT * FROM mass WHERE `keyarr` = '$arrkey1'"));
+$elem_2 = mysqli_fetch_row(mysqli_query($db, "SELECT * FROM mass WHERE `keyarr` = '$arrkey2'"));
 $tmp = $elem_2[2];
-mysqli_query($db, "UPDATE mass SET `val` = '$elem_1[2]' WHERE `keyarr` = '$arr_key_2'");
-mysqli_query($db, "UPDATE mass SET `val` = '$tmp' WHERE `keyarr` = '$arr_key_1'");
+mysqli_query($db, "UPDATE mass SET `val` = '$elem_1[2]' WHERE `keyarr` = '$arrkey2'");
+mysqli_query($db, "UPDATE mass SET `val` = '$tmp' WHERE `keyarr` = '$arrkey1'");
